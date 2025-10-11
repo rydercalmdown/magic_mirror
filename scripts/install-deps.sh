8fa580f7bbf7343213b85028388f35ea4ad898c0
ryderdamen <dev@ryderdamen.com>
2025-10-11 13:20:53 -0400

    adding test script

---
+++ scripts/install-deps.sh
@@ -0,0 +1,11 @@
+sudo apt update
+
+# Install OpenCV and dependencies
+sudo apt install -y python3-opencv libopencv-dev
+
+# Install additional dependencies for Node.js OpenCV
+sudo apt install -y build-essential cmake pkg-config
+sudo apt install -y libjpeg-dev libtiff5-dev libpng-dev
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt install -y libxvidcore-dev libx264-dev
+sudo apt install -y libgtk-3-dev libatlas-base-dev gfortran
